1f6729848d6dcb8132b33a35b599b903b47a8c68
Kalpesh Borde <bordekalpesh@gmail.com>
2025-07-12 10:17:08 +0530

    Auth done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,28 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
-const app = express()
+// Load env
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000)
+// Routes
+import authRoutes from "./routes/auth.js";
+
+
+app.use("/api/auth", authRoutes);
+
+
+// DB Connect
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.error("❌ Mongo Error:", err));
+
+// Start server
+app.listen(process.env.PORT, () => {
+  console.log(`✅ Server running on port ${process.env.PORT}`);
+});
